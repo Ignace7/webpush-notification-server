7deec4ac0b0e53a936523436500000f63a9c2274
SEO Ignacio Acevedo <124623437+Ignace7@users.noreply.github.com>
2025-05-12 02:21:02 -0700

    Delete src directory

--- src/server/routing/index.js
+++
@@ -1,9 +0,0 @@
-import routes from './routes';
-
-const setRouting = app => {
-  for (const route of routes) {
-    app[route.method](route.url, route.handler);
-  }
-};
-
-export default setRouting;
